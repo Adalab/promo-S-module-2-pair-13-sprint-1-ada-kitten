b114ce1ce2fa11066377f055309d38205cc7504b
Alexandra Campo <alexandracampo86@gmail.com>
2023-02-09 11:22:01 +0100

    objetos

--- js/main.js
+++ js/main.js
@@ -7,6 +7,44 @@ form.classList.remove("collapsed"); */
 
 const sectionList = document.querySelector(".js-list");
 
+// Ejercicio OBJETOS:
+
+
+const kittenData_1 = {
+  image: 'https://dev.adalab.es/gato-siames.webp',
+  name: 'Anastacio',
+  desc: 'Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.',
+  race: 'Siamés',
+};
+
+const kittenData_2 = {
+  image: 'https://dev.adalab.es/sphynx-gato.webp',
+  name: 'Fiona',
+  desc: 'Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.',
+  race: 'Sphynx',
+};
+
+const kittenData_3 = {
+  image: 'https://dev.adalab.es/maine-coon-cat.webp',
+  name: 'Cielo',
+  desc: ' Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.',
+  race: 'Maine Coon',
+};
+
+function renderKitten(kittenData) {
+  const html = `<li class="card">
+  <article>
+    <img
+      class="card_img" src=${kittenData.image}
+    <h3 class="card_title">${kittenData.name}</h3>
+    <h4 class="card_race">${kittenData.race}</h4>
+    <p class="card_description">${kittenData.desc}</p>
+  </article>
+</li>`;
+
+  return html;
+}
+
 //kittenOne
 const kittenImage1 = 'https://dev.adalab.es/gato-siames.webp';
 const kittenName1 = 'Anastacio';
@@ -128,8 +166,9 @@ const inputName = document.querySelector('.js-input-name');
 const inputRace = document.querySelector('.js-race')
 const labelMessageError = document.querySelector('.js-label-error');
 
+btn.addEventListener('click', addNewKitten);
 
-btn.addEventListener('click', (event) => {
+function addNewKitten(event) {
   event.preventDefault();
   const valueDesc = inputDesc.value;
   const valuePhoto = inputPhoto.value;
@@ -150,7 +189,21 @@ btn.addEventListener('click', (event) => {
 </li>`;
     sectionList.innerHTML += kittenNew;
   }
-})
+}
+
+function renderKitten(url, desc, name, race) {
+  const kittenNew = `<li class="card">
+  <article>
+    <img
+      class="card_img" src=${valuePhoto}
+    <h3 class="card_title">${valueName}</h3>
+    <h4 class="card_race">${valueRace}</h4>
+    <p class="card_description">${valueDesc}</p>
+  </article>
+  </li>`;
+
+  return kittenNew;
+}
 
 //3.FORMULARIO: cancelar formulario:
 
@@ -180,7 +233,7 @@ newFormElement.addEventListener('click', handleClickNewCatForm);
 
 function handleClickNewCatForm(event) {
   event.preventDefault();
-  console.log ('hola');
+  console.log('hola');
   if (form.classList.contains('collapsed')) {
     showNewCatForm(); //llama a la función
 
